5a5d7893172b1676c453b1e5011f64b2bab5e762
Andrew Lu <awclu@ucdavis.edu>
2020-03-27 22:28:26 -0700

    added pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React ,{Component} from 'react';
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={AboutMe} />
+          <Route exact path="/portfolio" component={Portfolio} />     
+        </Switch>
+        <Footer />
       </div>
+    </Router>
     );
   }
 }
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render(){
+        <div>
+            hello
+        </div>
+    }
+}
+
+export default Home;
